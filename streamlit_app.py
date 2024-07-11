c9e3ff5107aedb7cbfda39c8c4330ef561650f72
Zeyad Al Mothafar <36289354+zeyadalmothafar@users.noreply.github.com>
2024-07-11 12:04:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from snowflake.snowpark.functions import col
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -27,6 +26,7 @@ ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:',my_dataframe,max_selections= 5
 
 )
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
 if ingredients_list:
     #st.write(ingredients_list)
